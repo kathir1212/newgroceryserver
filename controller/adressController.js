9e7ae85a132d9c080ec2286318542162968d8000
kathir1212 <kathirvel082@gmail.com>
2025-07-28 19:38:02 +0530

    deploy test

--- controller/adressController.js
+++ controller/adressController.js
@@ -25,37 +25,21 @@ catch(error){
 
 
 
-export const getAddress = async (req,res)=>{
+export const getAddress = async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) return res.status(401).json({ success: false, message: 'No token found' });
 
-    try{
-  const {userId} = req.body;
-  const address = await Address.find({userId})
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.userId;
 
-  
+    const address = await Address.find({ userId });
 
-  
+    res.json({ success: true, address });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
-
-
-   res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-
-
-    
-
-
-  res.json({success:true,address})
-    }
-    catch(error){
-  res.json({success:false , message:error.message})
-
-    }
-
-}
 
 
