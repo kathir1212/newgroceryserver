f734ced5b9665e5429a69d7a011d5090f69c7c0d
kathir1212 <kathirvel082@gmail.com>
2025-07-28 16:37:29 +0530

    deployment work

--- controller/userController.js
+++ controller/userController.js
@@ -27,7 +27,7 @@ export const register = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
- sameSite:  "none",
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
        maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -70,7 +70,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-       sameSite:  "none",
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
