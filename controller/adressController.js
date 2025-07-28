be19c9cb100ea5b71c69af00b1048c99673b5584
kathir1212 <kathirvel082@gmail.com>
2025-07-28 17:30:49 +0530

    deploy test

--- controller/adressController.js
+++ controller/adressController.js
@@ -11,6 +11,8 @@ try{
   console.log(req.body,"bobyby");
   
   const response = await Address.create({...address, userId})
+
+
   res.json({success:true , message:response})
 }
 catch(error){
@@ -26,6 +28,16 @@ export const getAddress = async (req,res)=>{
     try{
   const {userId} = req.body;
   const address = await Address.find({userId})
+
+   res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    
+
+
   res.json({success:true,address})
     }
     catch(error){
