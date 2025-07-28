6a89ec98a0587a4614b49c0e6cf0854b1add2dd4
kathir1212 <kathirvel082@gmail.com>
2025-07-28 17:46:53 +0530

    deploy test

--- controller/adressController.js
+++ controller/adressController.js
@@ -29,15 +29,21 @@ export const getAddress = async (req,res)=>{
   const {userId} = req.body;
   const address = await Address.find({userId})
 
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+  
+
+  
+
+  // const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
 
-   res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+  //  res.cookie('token', token, {
+  //     httpOnly: true,
+  //     secure: process.env.NODE_ENV === 'production',
+  //     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+  //     maxAge: 7 * 24 * 60 * 60 * 1000,
+  //   });
+
+  
     
 
 
